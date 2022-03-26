25d76d210c1eb4b3d544844bc81e1c3e8c4327e9
Programmer0098 <emailemasoud0098@gmail.com>
2022-03-26 05:46:10 +0430

    testhaste

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ console.log("hello ");
 alert("hi man ...");
 
 console.log("hello ");
+console.log("hello ");
+console.log("hello ");
+console.log("hello ");
+console.log("hello ");
+console.log("hello ");
+console.log("hello ");
+console.log("hello ");
 
 
 console.log("zart");
